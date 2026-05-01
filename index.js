9ece3bd418f99142d2a4640c52fba9194ee96b38
Gunjita Jain <gunjitadgw9@gmail.com>
2026-05-02 01:49:45 +0530

    First commit to my Integrating With HubSpot I practicum repository

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
+
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -11,15 +13,67 @@ app.use(express.json());
 const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://api.hubapi.com/crm/v3/objects/2-229053443/search",
+      {
+        filterGroups: [],
+        properties: ["name", "course", "age"]
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TOKEN}`,
+          "Content-Type": "application/json"
+        },
+      }
+    );
 
+    const data = response.data.results;
+
+    res.render("homepage", { data });
+
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.send("Error fetching data");
+  }
+});
 // * Code for Route 1 goes here
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
+app.get("/update-cobj", (req, res) => {
+  res.render("updates");
+});
 // * Code for Route 2 goes here
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+app.post("/update-cobj", async (req, res) => {
+  const { name, course, age } = req.body;
 
+  try {
+    await axios.post(
+      "https://api.hubapi.com/crm/v3/objects/2-229053443",
+      {
+        properties: {
+          name,
+          course,
+          age
+        }
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TOKEN}`,
+          "Content-Type": "application/json"
+        },
+      }
+    );
+    res.redirect("/");
+
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.send("Error saving data");
+  }
+});
 // * Code for Route 3 goes here
 
 /** 
